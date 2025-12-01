5d2288b73a910786b4ee233543d3b9514cce34fc
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-11-30 19:32:24 -0500

    Adding some screen resolution stuff

--- SovietSolitaire/Game1.cs
+++ SovietSolitaire/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace SovietSolitaire
 {
@@ -8,13 +9,29 @@ namespace SovietSolitaire
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+		private RenderTarget2D _renderTarget;
+		private Rectangle _renderDestination;
+		private float _scale = 1f;
 
-        public Game1()
+		private int _nativeWidth = 1280;
+		private int _nativeHeight = 800;
+		private bool _isResizing;
+		bool _isFullscreen = false;
+		bool _isBorderless = false;
+		int _width = 0;
+		int _height = 0;
+
+
+		public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-        }
+
+			Window.Title = "Soviet Solitaire";
+			Window.AllowUserResizing = true;
+			Window.ClientSizeChanged += OnClientSizeChanged;
+		}
 
         protected override void Initialize()
         {
@@ -48,5 +65,106 @@ namespace SovietSolitaire
 
             base.Draw(gameTime);
         }
-    }
+
+		private void OnClientSizeChanged(object sender, EventArgs e)
+		{
+			if (!_isResizing && Window.ClientBounds.Width > 0 && Window.ClientBounds.Height > 0)
+			{
+				_isResizing = true;
+
+				CalculateRenderDestination();
+
+				_isResizing = false;
+			}
+		}
+
+		private void CalculateRenderDestination()
+		{
+			Point size = GraphicsDevice.Viewport.Bounds.Size;
+
+			float scaleX = (float)size.X / _renderTarget.Width;
+			float scaleY = (float)size.Y / _renderTarget.Height;
+
+			_scale = Math.Min(scaleX, scaleY);
+
+			_renderDestination.Width = (int)(_renderTarget.Width * _scale);
+			_renderDestination.Height = (int)(_renderTarget.Height * _scale);
+
+			_renderDestination.X = (size.X - _renderDestination.Width) / 2;
+			_renderDestination.Y = (size.Y - _renderDestination.Height) / 2;
+		}
+
+		// Learn MonoGame how-to Fullscreen and Borderless code
+		public void ToggleFullscreen()
+		{
+			bool oldIsFullscreen = _isFullscreen;
+
+			if (_isBorderless)
+			{
+				_isBorderless = false;
+			}
+			else
+			{
+				_isFullscreen = !_isFullscreen;
+			}
+
+			ApplyFullscreenChange(oldIsFullscreen);
+		}
+		public void ToggleBorderless()
+		{
+			bool oldIsFullscreen = _isFullscreen;
+
+			_isBorderless = !_isBorderless;
+			_isFullscreen = _isBorderless;
+
+			ApplyFullscreenChange(oldIsFullscreen);
+		}
+
+		private void ApplyFullscreenChange(bool oldIsFullscreen)
+		{
+			if (_isFullscreen)
+			{
+				if (oldIsFullscreen)
+				{
+					ApplyHardwareMode();
+				}
+				else
+				{
+					SetFullscreen();
+				}
+			}
+			else
+			{
+				UnsetFullscreen();
+			}
+		}
+		private void ApplyHardwareMode()
+		{
+			_graphics.HardwareModeSwitch = !_isBorderless;
+			_graphics.ApplyChanges();
+		}
+		private void SetFullscreen()
+		{
+			_width = Window.ClientBounds.Width;
+			_height = Window.ClientBounds.Height;
+
+			_graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+			_graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+			// _graphics.HardwareModeSwitch = !_isBorderless;
+			_graphics.HardwareModeSwitch = false;
+
+			_graphics.IsFullScreen = true;
+			_graphics.ApplyChanges();
+		}
+		private void UnsetFullscreen()
+		{
+			// Reset to default resolution, but we do have the previous Width and Height just tweaky if somebody resized
+			_graphics.PreferredBackBufferWidth = _nativeWidth;
+			_graphics.PreferredBackBufferHeight = _nativeHeight;
+
+			_graphics.HardwareModeSwitch = false;
+			_graphics.IsFullScreen = false;
+			_graphics.ApplyChanges();
+		}
+	}
 }
