74770d2c94b0954cdc4332f6ac46ddced4ce4c39
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-11-30 19:35:15 -0500

    Refactor and make re-size work

--- SovietSolitaire/Game1.cs
+++ SovietSolitaire/Game1.cs
@@ -3,168 +3,169 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 
-namespace SovietSolitaire
+namespace SovietSolitaire;
+
+public class Game1 : Game
 {
-    public class Game1 : Game
-    {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-		private RenderTarget2D _renderTarget;
-		private Rectangle _renderDestination;
-		private float _scale = 1f;
+	private GraphicsDeviceManager _graphics;
+	private SpriteBatch _spriteBatch;
+	private RenderTarget2D _renderTarget;
+	private Rectangle _renderDestination;
+	private float _scale = 1f;
 
-		private int _nativeWidth = 1280;
-		private int _nativeHeight = 800;
-		private bool _isResizing;
-		bool _isFullscreen = false;
-		bool _isBorderless = false;
-		int _width = 0;
-		int _height = 0;
+	private int _nativeWidth = 1280;
+	private int _nativeHeight = 800;
+	private bool _isResizing;
+	bool _isFullscreen = false;
+	bool _isBorderless = false;
+	int _width = 0;
+	int _height = 0;
 
 
-		public Game1()
-        {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+	public Game1()
+	{
+		_graphics = new GraphicsDeviceManager(this);
+		Content.RootDirectory = "Content";
+		IsMouseVisible = true;
 
-			Window.Title = "Soviet Solitaire";
-			Window.AllowUserResizing = true;
-			Window.ClientSizeChanged += OnClientSizeChanged;
+		Window.Title = "Soviet Solitaire";
+		Window.AllowUserResizing = true;
+		Window.ClientSizeChanged += OnClientSizeChanged;
+	}
+
+	protected override void Initialize()
+	{
+		base.Initialize();
+
+		_renderTarget = new RenderTarget2D(GraphicsDevice, _nativeWidth, _nativeHeight);
+
+		CalculateRenderDestination();
+	}
+
+	protected override void LoadContent()
+	{
+		_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+		// TODO: use this.Content to load your game content here
+	}
+
+	protected override void Update(GameTime gameTime)
+	{
+		if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			Exit();
+
+		// TODO: Add your update logic here
+
+		base.Update(gameTime);
+	}
+
+	protected override void Draw(GameTime gameTime)
+	{
+		GraphicsDevice.Clear(Color.CornflowerBlue);
+
+		// TODO: Add your drawing code here
+
+		base.Draw(gameTime);
+	}
+
+	private void OnClientSizeChanged(object sender, EventArgs e)
+	{
+		if (!_isResizing && Window.ClientBounds.Width > 0 && Window.ClientBounds.Height > 0)
+		{
+			_isResizing = true;
+
+			CalculateRenderDestination();
+
+			_isResizing = false;
+		}
+	}
+
+	private void CalculateRenderDestination()
+	{
+		Point size = GraphicsDevice.Viewport.Bounds.Size;
+
+		float scaleX = (float)size.X / _renderTarget.Width;
+		float scaleY = (float)size.Y / _renderTarget.Height;
+
+		_scale = Math.Min(scaleX, scaleY);
+
+		_renderDestination.Width = (int)(_renderTarget.Width * _scale);
+		_renderDestination.Height = (int)(_renderTarget.Height * _scale);
+
+		_renderDestination.X = (size.X - _renderDestination.Width) / 2;
+		_renderDestination.Y = (size.Y - _renderDestination.Height) / 2;
+	}
+
+	// Learn MonoGame how-to Fullscreen and Borderless code
+	public void ToggleFullscreen()
+	{
+		bool oldIsFullscreen = _isFullscreen;
+
+		if (_isBorderless)
+		{
+			_isBorderless = false;
+		}
+		else
+		{
+			_isFullscreen = !_isFullscreen;
 		}
 
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+		ApplyFullscreenChange(oldIsFullscreen);
+	}
+	public void ToggleBorderless()
+	{
+		bool oldIsFullscreen = _isFullscreen;
 
-            base.Initialize();
-        }
+		_isBorderless = !_isBorderless;
+		_isFullscreen = _isBorderless;
 
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+		ApplyFullscreenChange(oldIsFullscreen);
+	}
 
-            // TODO: use this.Content to load your game content here
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
-        }
-
-		private void OnClientSizeChanged(object sender, EventArgs e)
+	private void ApplyFullscreenChange(bool oldIsFullscreen)
+	{
+		if (_isFullscreen)
 		{
-			if (!_isResizing && Window.ClientBounds.Width > 0 && Window.ClientBounds.Height > 0)
+			if (oldIsFullscreen)
 			{
-				_isResizing = true;
-
-				CalculateRenderDestination();
-
-				_isResizing = false;
-			}
-		}
-
-		private void CalculateRenderDestination()
-		{
-			Point size = GraphicsDevice.Viewport.Bounds.Size;
-
-			float scaleX = (float)size.X / _renderTarget.Width;
-			float scaleY = (float)size.Y / _renderTarget.Height;
-
-			_scale = Math.Min(scaleX, scaleY);
-
-			_renderDestination.Width = (int)(_renderTarget.Width * _scale);
-			_renderDestination.Height = (int)(_renderTarget.Height * _scale);
-
-			_renderDestination.X = (size.X - _renderDestination.Width) / 2;
-			_renderDestination.Y = (size.Y - _renderDestination.Height) / 2;
-		}
-
-		// Learn MonoGame how-to Fullscreen and Borderless code
-		public void ToggleFullscreen()
-		{
-			bool oldIsFullscreen = _isFullscreen;
-
-			if (_isBorderless)
-			{
-				_isBorderless = false;
-			}
-			else
-			{
-				_isFullscreen = !_isFullscreen;
-			}
-
-			ApplyFullscreenChange(oldIsFullscreen);
-		}
-		public void ToggleBorderless()
-		{
-			bool oldIsFullscreen = _isFullscreen;
-
-			_isBorderless = !_isBorderless;
-			_isFullscreen = _isBorderless;
-
-			ApplyFullscreenChange(oldIsFullscreen);
-		}
-
-		private void ApplyFullscreenChange(bool oldIsFullscreen)
-		{
-			if (_isFullscreen)
-			{
-				if (oldIsFullscreen)
-				{
-					ApplyHardwareMode();
-				}
-				else
-				{
-					SetFullscreen();
-				}
+				ApplyHardwareMode();
 			}
 			else
 			{
-				UnsetFullscreen();
+				SetFullscreen();
 			}
 		}
-		private void ApplyHardwareMode()
+		else
 		{
-			_graphics.HardwareModeSwitch = !_isBorderless;
-			_graphics.ApplyChanges();
+			UnsetFullscreen();
 		}
-		private void SetFullscreen()
-		{
-			_width = Window.ClientBounds.Width;
-			_height = Window.ClientBounds.Height;
+	}
+	private void ApplyHardwareMode()
+	{
+		_graphics.HardwareModeSwitch = !_isBorderless;
+		_graphics.ApplyChanges();
+	}
+	private void SetFullscreen()
+	{
+		_width = Window.ClientBounds.Width;
+		_height = Window.ClientBounds.Height;
 
-			_graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-			_graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-			// _graphics.HardwareModeSwitch = !_isBorderless;
-			_graphics.HardwareModeSwitch = false;
+		_graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+		_graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+		// _graphics.HardwareModeSwitch = !_isBorderless;
+		_graphics.HardwareModeSwitch = false;
 
-			_graphics.IsFullScreen = true;
-			_graphics.ApplyChanges();
-		}
-		private void UnsetFullscreen()
-		{
-			// Reset to default resolution, but we do have the previous Width and Height just tweaky if somebody resized
-			_graphics.PreferredBackBufferWidth = _nativeWidth;
-			_graphics.PreferredBackBufferHeight = _nativeHeight;
+		_graphics.IsFullScreen = true;
+		_graphics.ApplyChanges();
+	}
+	private void UnsetFullscreen()
+	{
+		// Reset to default resolution, but we do have the previous Width and Height just tweaky if somebody resized
+		_graphics.PreferredBackBufferWidth = _nativeWidth;
+		_graphics.PreferredBackBufferHeight = _nativeHeight;
 
-			_graphics.HardwareModeSwitch = false;
-			_graphics.IsFullScreen = false;
-			_graphics.ApplyChanges();
-		}
+		_graphics.HardwareModeSwitch = false;
+		_graphics.IsFullScreen = false;
+		_graphics.ApplyChanges();
 	}
 }
