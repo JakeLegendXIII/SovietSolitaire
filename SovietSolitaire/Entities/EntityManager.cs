391826cd37b1135bcdc41e49bfa25bbb4fd9d151
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-12-04 23:18:44 -0500

    Setup EntityManager

---
+++ SovietSolitaire/Entities/EntityManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SovietSolitaire.Library;
+
+namespace SovietSolitaire.Entities;
+
+public class EntityManager : IGameEntity
+{
+	// TODO : List of Slots that have list of cards
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		
+	}
+}
