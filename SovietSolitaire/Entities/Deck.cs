2b2cf4535c6e19ced7fdd67ece74fd7cf1ce9ce2
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-12-04 20:32:31 -0500

    Start setting up Entities

---
+++ SovietSolitaire/Entities/Deck.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SovietSolitaire.Library;
+
+namespace SovietSolitaire.Entities;
+
+public class Deck : IGameEntity
+{
+
+
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		
+	}
+}
