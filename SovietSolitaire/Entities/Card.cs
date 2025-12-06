e7cf02f4ff5b606aaa95740cd42df5408095136a
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-12-05 21:50:44 -0500

    Create Card shell

---
+++ SovietSolitaire/Entities/Card.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SovietSolitaire.Library;
+using System;
+
+namespace SovietSolitaire.Entities;
+
+internal class Card : IGameEntity
+{
+
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		
+	}
+}
