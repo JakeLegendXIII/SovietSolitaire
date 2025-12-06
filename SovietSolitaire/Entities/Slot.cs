211f926981dfedb924279ace75a001390716b601
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-12-05 21:52:02 -0500

    Add Slot shell

---
+++ SovietSolitaire/Entities/Slot.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SovietSolitaire.Library;
+
+namespace SovietSolitaire.Entities;
+
+internal class Slot : IGameEntity
+{
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		
+	}
+}
