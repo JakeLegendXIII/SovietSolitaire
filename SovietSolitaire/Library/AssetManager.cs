38991ca5e12ae29ec7005ae11979923b9cee305a
JakeLegendXIII <jakelegendxiii@gmail.com>
2026-09-12 00:46:18 -0400

    Add second font and UI banner

--- SovietSolitaire/Library/AssetManager.cs
+++ SovietSolitaire/Library/AssetManager.cs
@@ -6,13 +6,17 @@ namespace SovietSolitaire.Library;
 public static class AssetManager
 {
 	public static SpriteFont ArmadaFont { get; private set; }
+	public static SpriteFont FarawayFont { get; private set; }
 	public static Texture2D Cards {  get; private set; }
 	public static Texture2D ButtonUI { get; private set; }
+	public static Texture2D ConfirmationBanner { get; private set; }
 
 	public static void Load(ContentManager content)
 	{
 		ArmadaFont = content.Load<SpriteFont>("Fonts/ArmadaBold16");
+		FarawayFont = content.Load<SpriteFont>("Fonts/Faraway16");
 		Cards = content.Load<Texture2D>("Sprites/Cards");
 		ButtonUI = content.Load<Texture2D>("UI/MenuButton");
+		ConfirmationBanner = content.Load<Texture2D>("UI/ConfirmationBanner");
 	}
 }
