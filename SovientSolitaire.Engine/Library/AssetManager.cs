31db1728c8a7928970ec139327c47b3b63e1893b
Jake Kirkbride <jakelegendxiii@gmail.com>
2026-09-21 17:39:34 -0400

    Add to DesktopGL project

--- SovientSolitaire.Engine/Library/AssetManager.cs
+++ SovientSolitaire.Engine/Library/AssetManager.cs
@@ -8,6 +8,10 @@ public static class AssetManager
 	public static SpriteFont ArmadaFont { get; private set; }
 	public static SpriteFont FarawayFont { get; private set; }
 	public static Texture2D Cards {  get; private set; }
+	public static Texture2D ClubCards { get; private set; }
+	public static Texture2D DiamondCards { get; private set; }
+	public static Texture2D HeartCards { get; private set; }
+	public static Texture2D SpadeCards { get; private set; }
 	public static Texture2D ButtonUI { get; private set; }
 	public static Texture2D ConfirmationBanner { get; private set; }
 
@@ -16,6 +20,10 @@ public static class AssetManager
 		ArmadaFont = content.Load<SpriteFont>("Fonts/ArmadaBold16");
 		FarawayFont = content.Load<SpriteFont>("Fonts/Faraway16");
 		Cards = content.Load<Texture2D>("Sprites/Cards");
+		ClubCards = content.Load<Texture2D>("Sprites/Club_Cards");
+		DiamondCards = content.Load<Texture2D>("Sprites/Diamond_Cards");
+		HeartCards = content.Load<Texture2D>("Sprites/Heart_Cards");
+		SpadeCards = content.Load<Texture2D>("Sprites/Spade_Cards");
 		ButtonUI = content.Load<Texture2D>("UI/MenuButton");
 		ConfirmationBanner = content.Load<Texture2D>("UI/ConfirmationBanner");
 	}
