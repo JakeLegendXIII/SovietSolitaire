9bd0ee10950a7d02b27bf3f8f80f5b80a852ae45
Jake Kirkbride <jakelegendxiii@gmail.com>
2026-09-21 00:00:39 -0400

    Note

--- SovientSolitaire.Engine/Entities/EntityManager.cs
+++ SovientSolitaire.Engine/Entities/EntityManager.cs
@@ -10,6 +10,7 @@ namespace SovietSolitaire.Engine.Entities;
 
 public class EntityManager : IGameEntity
 {
+	// TODO : Resize card assets
 	private const int ScreenWidth = 1280;
 	private const int ScreenHeight = 800;
 	private const int SlotCount = 9;
